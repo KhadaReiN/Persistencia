9d99e50a6fe687434e544572d73f1f6f1cc59ba1
KhadaReiN <96033269+KhadaReiN@users.noreply.github.com>
2024-03-08 13:45:50 -0500

    Delete persistencia directory

--- persistencia/src/main/java/edu/unimagdalena/persistencia/repository/SugerenciaRepo.java
+++
@@ -1,6 +0,0 @@
-package edu.unimagdalena.persistencia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.unimagdalena.persistencia.entidades.Sugerencia;
-
-public interface SugerenciaRepo extends JpaRepository<Sugerencia,Long>{}
--- persistencia/src/main/java/edu/unimagdalena/persistencia/repository/UsuarioRepo.java
+++
@@ -1,6 +0,0 @@
-package edu.unimagdalena.persistencia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import edu.unimagdalena.persistencia.entidades.Usuario;
-
-public interface UsuarioRepo extends JpaRepository<Usuario,Long>{}
